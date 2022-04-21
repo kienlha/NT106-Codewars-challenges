dcc7f793390d5609ddd5f71e7692c1cb90ca3e6f
kienlha <74295186+kienlha@users.noreply.github.com>
2022-04-21 15:17:21 +0700

    Update group8-solution.cs

--- group8-solution.cs
+++ group8-solution.cs
@@ -33,3 +33,10 @@ public static int[] Maps(int[] x)
 
     return x;
 }
+//It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+//You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+public static string Remove_char(string s)
+            {
+                s = s.Substring(1, s.Length - 2);
+                return s;
+            }
